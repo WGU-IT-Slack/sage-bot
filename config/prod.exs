7084800fb4cbbaa4d4c34f6e2b8a8d3d9b144766
DanMHammer <35697323+DanMHammer@users.noreply.github.com>
2018-09-04 22:59:43 -0500

    Added Phoenix build pack on heroku
    Testing. Probably will fail Travis

--- config/prod.exs
+++ config/prod.exs
@@ -5,3 +5,20 @@ config :sage, Sage.Robot,
   name: System.get_env("SLACK_BOT"),
   aka: System.get_env("SLACK_BOT_ALIAS"),
   token: System.get_env("SLACK_TOKEN")
+
+config :hello, HelloWeb.Endpoint,
+  load_from_system_env: true,
+  url: [scheme: "https", host: "sage-bot.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+# Do not print debug messages in production
+config :logger, level: :info
+
+# Configure your database
+config :hello, Hello.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
