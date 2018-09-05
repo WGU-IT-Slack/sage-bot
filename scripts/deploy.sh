1b2085f35fda62102be59c544cfa3dcc4c0f1fa2
DanMHammer <35697323+DanMHammer@users.noreply.github.com>
2018-09-05 14:10:40 -0500

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 id_rsa # Allow read access to the private key
-ssh-add id_rsa # Add the private key to SSH
+chmod 600 ~/id_rsa # Allow read access to the private key
+ssh-add ~/id_rsa # Add the private key to SSH
 git config --global push.default matching
 git remote add deploy ssh://travis@$IP:$PORT$DEPLOY_DIR
 git push deploy DanMHammer-VPS
